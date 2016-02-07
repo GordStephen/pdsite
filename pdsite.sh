a6d6a91eea5eec3683c3c5405093a97de0974b1f
Gord Stephen <gord@gordstephen.ca>
2016-02-07 16:36:53 -0500

    Preliminary header/footer templating

--- pdsite.sh
+++ pdsite.sh
@@ -1,16 +1,42 @@
-inputform=.md #TODO: Switch to this
+templatespath=~/Projects/pdsite
+
+inputextension=.md
 outputfolder=.html
+template=test-template
+
+templatepath=$templatespath/$template
+
+# Convert to canonical path
+outputfolder=$(readlink -f $outputfolder)
+
+# Build glob expressions
+extensionglob='*'$inputextension
+indexfileglob='*index'$inputextension
 
 # Purge output folder
 rm -rf $outputfolder
 
 # Generate base file structure
-find -not -path "*/\.*" -not -path "*index.md" -path '*.md' -type f | sed 's/\(.*\).md/\1/' | xargs -I path mkdir -p .html/path
+find -not -path "*/\.*" -not -path "$indexfileglob" -path "$extensionglob" -type f | sed 's/\(.*\)'$inputextension'/\1/' | xargs -I path mkdir -p $outputfolder/path
 
-# Copy over index files
-find -not -path "*/\.*" -path '*index.md'  -type f -exec cp {} .html/{} \;
+# Copy in index files
+find -not -path "*/\.*" -path "$indexfileglob"  -type f -exec cp {} $outputfolder/{} \;
 
-# Copy over other files
-find -not -path "*/\.*" -not -path "*index.md" -path '*.md' -type f | sed 's/\(.*\)\/\(.*\).md/\0 \1\/\2\/index.md/' |  xargs -n 2 sh -c 'cp $0 .html/$1'
+# Copy in other files
+find -not -path "*/\.*" -not -path "$indexfileglob" -path "$extensionglob" -type f | sed 's/\(.*\)\/\(.*\)'$inputextension'/\0 \1\/\2\/index'$inputextension'/' |  xargs -n 2 sh -c 'cp $0 '$outputfolder'/$1'
 
-tree $outputfolder
+# Generate file structure for navigation templates
+echo '---\n' $(tree -dJ --noreport $outputfolder | cut -c 2-) '\n...' > $outputfolder/tree.yaml
+
+# Generate header files
+find $outputfolder -path "$indexfileglob" -type f -execdir pandoc -o header.html --template $templatepath/header.html $outputfolder/tree.yaml \;
+
+# Generate footer files
+find $outputfolder -path "$indexfileglob" -type f -execdir pandoc -o footer.html --template $templatepath/footer.html $outputfolder/tree.yaml \;
+
+# Convert content files to HTML
+find $outputfolder -path "$indexfileglob" -type f -execdir pandoc -B header.html -A footer.html -o {}.html {} \; -delete
+
+
+# TODO: Generate header/footer HTML from templates
+# TODO: Copy in css/js/img
