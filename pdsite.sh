0327d94aaa95ccb9c53d7169f1523774f4188e40
Gord Stephen <gord@gordstephen.ca>
2016-02-07 17:50:48 -0500

    Fixed YAML generation bug and added rudimentary navigation to header

--- pdsite.sh
+++ pdsite.sh
@@ -1,8 +1,8 @@
-templatespath=~/Projects/pdsite
+templatespath=~/Projects/pdsite/themes
 
 inputextension=.md
 outputfolder=.html
-template=test-template
+template=default
 
 templatepath=$templatespath/$template
 
@@ -14,8 +14,11 @@ extensionglob='*'$inputextension
 indexfileglob='*index'$inputextension
 
 # Purge output folder
+echo -n "Clearing old build folder..."
 rm -rf $outputfolder
+echo " done."
 
+echo -n "Building site..."
 # Generate base file structure
 find -not -path "*/\.*" -not -path "$indexfileglob" -path "$extensionglob" -type f | sed 's/\(.*\)'$inputextension'/\1/' | xargs -I path mkdir -p $outputfolder/path
 
@@ -26,7 +29,7 @@ find -not -path "*/\.*" -path "$indexfileglob"  -type f -exec cp {} $outputfolde
 find -not -path "*/\.*" -not -path "$indexfileglob" -path "$extensionglob" -type f | sed 's/\(.*\)\/\(.*\)'$inputextension'/\0 \1\/\2\/index'$inputextension'/' |  xargs -n 2 sh -c 'cp $0 '$outputfolder'/$1'
 
 # Generate file structure for navigation templates
-echo '---\n' $(tree -dJ --noreport $outputfolder | cut -c 2-) '\n...' > $outputfolder/tree.yaml
+echo -e '---\n' $(tree -dJ --noreport $outputfolder | cut -c 2-) '\n...' > $outputfolder/tree.yaml
 
 # Generate header files
 find $outputfolder -path "$indexfileglob" -type f -execdir pandoc -o header.html --template $templatepath/header.html $outputfolder/tree.yaml \;
@@ -35,8 +38,10 @@ find $outputfolder -path "$indexfileglob" -type f -execdir pandoc -o header.html
 find $outputfolder -path "$indexfileglob" -type f -execdir pandoc -o footer.html --template $templatepath/footer.html $outputfolder/tree.yaml \;
 
 # Convert content files to HTML
-find $outputfolder -path "$indexfileglob" -type f -execdir pandoc -B header.html -A footer.html -o {}.html {} \; -delete
+find $outputfolder -path "$indexfileglob" -type f -execdir pandoc -B header.html -A footer.html -o index.html {} \; -delete
 
-
-# TODO: Generate header/footer HTML from templates
 # TODO: Copy in css/js/img
+echo " done."
+
+echo "Starting up server at localhost:8000..."
+webfsd -F -r $outputfolder -f index.html -l -
